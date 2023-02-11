5e0ee5be4c58ceaaf04b1609623427df71ffa96c
KORGA <nikita-gon1@mail.ru>
2023-02-11 08:36:48 +0300

    Лаба готова

--- main.cpp
+++ main.cpp
@@ -1,37 +1,35 @@
 #include <iostream>
 #include<array>
 #include <vector>
-#include "Stack.h"
 
 using namespace std;
 
 // Задание 1
-// Опишите параметризованную функцию сортировки вставкой.
-template<class T>
-vector<T> insertionSort(vector<T> arr) {
-    vector<T> array = arr;
-    int n = array.size();
-    for (int i = 1; i < n; i++)
-        for (int j = i; j > 0 && array[j - 1] > array[j]; j--)
-            swap(array[j - 1], array[j]);
-    return array;
-}
+// Выполнить задачу в соответствии с номером варианта. При выполнении задачи использовать функциональные объекты.
+// Задание 2
+// Повторить выполнение своего варианта задания, но с использованием параметризованных классов.
 
-// Вспомогательная функция для печати векторов
+// Дан целочисленный вектор. Используя функцию transform умножить на масштабирующий
+// коэффициент все элементы вектора, не превышающие заданного числа.
+
 template<class T>
-void print(vector<T> arr) {
-    for (auto i: arr) cout << i << " ";
-}
+class Transform {
+    vector<T> arr;
+public:
+    Transform(vector<T> arr) : arr(arr) {};
+
+    vector<T> operator()(int coefficient, int threshold) {
+        for (int i = 0; i < arr.size(); i++){
+            if (arr[i] < threshold) arr[i] *= coefficient;
+        }
+        return arr;
+    }
+};
 
 int main() {
-    //Задание 1
-    vector<int> arr = {1, 9, 2, 4, 8, 3};
-    print(insertionSort(arr));
-
-    //Задание 2
-    //Опишите оператор [] для разработанного класса-массива, обработайте ошибку выхода за границы массива.
-    Stack stack;
-    cout << stack[10];
+    vector <char> arr = {'a', 'b', 'c', 'b'};
+    vector <char> result = Transform(arr)(5,'c');
+    for (auto  i : result) cout << i << " ";
 }
 
 
